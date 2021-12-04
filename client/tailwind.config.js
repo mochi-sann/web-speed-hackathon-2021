ec277462977eb2763c4f05316c2215a102ff5698
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2021-12-04 12:29:42 +0900

    画像圧縮とtailwindcssの圧縮をした

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   darkMode: false,
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
 };
